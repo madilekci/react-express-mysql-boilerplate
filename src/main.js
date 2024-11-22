bb8190652ea616b44cefba2a8287a989e1563b07
Muhammed Ali <madilekci@gmail.com>
2024-11-23 02:45:58 +0300

    Convert files to `.js`

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './pages/Home.jsx';
+import App from './pages/Home.js';
 import './index.css';
-import { BackendProvider } from './context/backend-context.jsx';
+import { BackendProvider } from './context/backend-context.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
