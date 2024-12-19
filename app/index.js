228553274b074cd0ed8d63a797d93d3e2e92ab0c
Muhammed Ali <36687970+madilekci@users.noreply.github.com>
2024-12-19 21:47:46 +0300

    Move startup code to a function & export the `app`

--- app/index.js
+++ app/index.js
@@ -12,31 +12,40 @@ const __dirname = path.dirname(__filename);
 
 import routes from './routes/index.js';
 
-const app = express();
+const startExpressServer = () => {
+    const app = express();
 
-const PORT = process.env.PORT || 8081;
-const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
-console.log('client url', CLIENT_URL);
+    const PORT = process.env.PORT || 8081;
+    const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
+    console.log('client url', CLIENT_URL);
 
-const corsOptions = { origin: CLIENT_URL };
+    const corsOptions = { origin: CLIENT_URL };
 
-app.use(cors(corsOptions));
-app.use(json({ limit: '20mb' }));
-app.use(urlencoded({ extended: true }));
-app.use('/', routes);
+    app.use(cors(corsOptions));
+    app.use(json({ limit: '20mb' }));
+    app.use(urlencoded({ extended: true }));
+    app.use('/', routes);
 
-db.sequelize.sync();
+    db.sequelize.sync();
 
-// Serve the static files from the Vite build directory
-const buildPath = path.resolve(__dirname, 'dist');
-app.use(express.static(buildPath));
+    // Serve the static files from the Vite build directory
+    const buildPath = path.resolve(__dirname, 'dist');
+    app.use(express.static(buildPath));
 
-// Fallback to serve `index.html` for React routing
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(buildPath, 'index.html'));
-});
+    // Fallback to serve `index.html` for React routing
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(buildPath, 'index.html'));
+    });
 
-// set port, listen for requests
-app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}.`);
-});
+    // set port, listen for requests
+    app.listen(PORT, () => {
+        console.log(`App is running on port ${PORT}.`);
+    });
+
+    return app;
+};
+
+// Start the Express server
+startExpressServer();
+
+export default startExpressServer;
