5a037bac2fef7583159e0965625ff25aca17b9e8
Muhammed Ali <36687970+madilekci@users.noreply.github.com>
2024-12-19 23:24:18 +0300

    Add spinner when waiting for API response

--- src/pages/AquaGSM.js
+++ src/pages/AquaGSM.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import { Button, Input, Col, Divider } from 'antd';
+import { Button, Input, Col, Divider, Spin } from 'antd';
 
 import Utils from '../helpers/utils.js';
 import { useBackend } from '../context/backend-context.js';
 import AquaGSMTable from '../components/AquaGSMTable.js';
 
 function AquaGSM() {
+    const [loading, setLoading] = useState(false);
     const backend = useBackend();
     const [aquaGSMs, setAquaGSMs] = useState([]);
     const [filter, setFilter] = useState({
@@ -13,19 +14,28 @@ function AquaGSM() {
         GSM: undefined
     });
 
-    async function fetchData(filter = {}) {
-        filter = Utils.removeEmptyStrings(filter);
-        const response = await backend.post(
-            'aquaGSM',
-            {
-                filter: filter ? { ...filter } : undefined,
-            },
-            false
-        );
-        if (response) {
-            setAquaGSMs(response);
+    const fetchData = async(filter = {}) => {
+        try {
+            setLoading(true);
+            filter = Utils.removeEmptyStrings(filter);
+            const response = await backend.post(
+                'aquaGSM',
+                {
+                    filter: filter ? { ...filter } : undefined,
+                },
+                false
+            );
+            if (response) {
+                setAquaGSMs(response);
+            }
         }
-    }
+        catch (error) {
+            console.error(error);
+        }
+        finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <div style={{ minHeight: '700px' }}>
@@ -36,9 +46,17 @@ function AquaGSM() {
             <Col span={6} style={{ marginBottom: '8px' }}>
                 <Input placeholder='GSM' onChange={e => setFilter({ ...filter, GSM: e.target.value })} />
             </Col>
-            <Button type='primary' onClick={() => fetchData(filter)} > Search </Button>
-            <Divider/>
-            <AquaGSMTable dataSource={aquaGSMs} />
+            <Button type="primary" disabled={loading} onClick={() => fetchData(filter)}>
+				Search
+            </Button>
+            <Divider />
+            {loading ? (
+                <Spin tip="Yükleniyor... Lütfen Bekleyiniz!">
+                    <div style={{ minHeight: '300px' }}></div>{' '}
+                </Spin>
+            ) : (
+                <AquaGSMTable dataSource={aquaGSMs} />
+            )}
         </div>
     );
 }
